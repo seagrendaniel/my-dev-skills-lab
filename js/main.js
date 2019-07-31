1014c6a8316839bdfc586313c7c5b822a4b359cf
blackdanamite <seagrendaniel@gmail.com>
2019-07-31 16:26:22 -0700

    finishes final jQuery logic including removing skills

--- js/main.js
+++ js/main.js
@@ -3,9 +3,17 @@ var addSkill = $('.add-skill-btn');
 
 
 addSkill.on('click', function(){
-    console.log($('.input-box').val());
     let skill = $('.input-box').val();
     $('tbody')
         .append(`<tr>
         <td class="btn"><button class="remove-btn"><image src="https://www.sccpre.cat/mypng/full/23-233226_computer-icons-icon-design-x-mark-download-checkbox.png"></image></button></td><td class="skill">${skill}</td>`);
+
+    $('.input-box').val('');
+    });
+
+
+$(document).on('click', '.remove-btn', function(){
+    $(this).closest('tr').fadeOut(500, function(){
+        $(this).remove();
+    });
 });
